d14d401c842bcc06faf082785805bf3a3d2f4d41
Yaryna <jasja1231@gmail.com>
2015-03-22 07:47:58 +0100

    Changed Optional window

--- GradedP4Lab03/Options.Designer.cs
+++ GradedP4Lab03/Options.Designer.cs
@@ -28,21 +28,209 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.inputBox1 = new System.Windows.Forms.TextBox();
+            this.inputBox2 = new System.Windows.Forms.TextBox();
+            this.inputBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.NonStanrdard = new System.Windows.Forms.RadioButton();
+            this.Difficulty = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.Confirm = new System.Windows.Forms.Button();
+            this.Cancel = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Difficulty.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // inputBox1
+            // 
+            this.inputBox1.Location = new System.Drawing.Point(189, 51);
+            this.inputBox1.Name = "inputBox1";
+            this.inputBox1.Size = new System.Drawing.Size(100, 20);
+            this.inputBox1.TabIndex = 0;
+            // 
+            // inputBox2
+            // 
+            this.inputBox2.Location = new System.Drawing.Point(189, 77);
+            this.inputBox2.Name = "inputBox2";
+            this.inputBox2.Size = new System.Drawing.Size(100, 20);
+            this.inputBox2.TabIndex = 1;
+            // 
+            // inputBox3
+            // 
+            this.inputBox3.Location = new System.Drawing.Point(189, 103);
+            this.inputBox3.Name = "inputBox3";
+            this.inputBox3.Size = new System.Drawing.Size(100, 20);
+            this.inputBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(148, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Height";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Width";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(148, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mines";
+            // 
+            // NonStanrdard
+            // 
+            this.NonStanrdard.AutoSize = true;
+            this.NonStanrdard.Location = new System.Drawing.Point(151, 19);
+            this.NonStanrdard.Name = "NonStanrdard";
+            this.NonStanrdard.Size = new System.Drawing.Size(86, 17);
+            this.NonStanrdard.TabIndex = 6;
+            this.NonStanrdard.TabStop = true;
+            this.NonStanrdard.Text = "Not standard";
+            this.NonStanrdard.UseVisualStyleBackColor = true;
+            // 
+            // Difficulty
+            // 
+            this.Difficulty.Controls.Add(this.radioButton1);
+            this.Difficulty.Controls.Add(this.radioButton3);
+            this.Difficulty.Controls.Add(this.radioButton2);
+            this.Difficulty.Controls.Add(this.inputBox1);
+            this.Difficulty.Controls.Add(this.NonStanrdard);
+            this.Difficulty.Controls.Add(this.inputBox2);
+            this.Difficulty.Controls.Add(this.label3);
+            this.Difficulty.Controls.Add(this.inputBox3);
+            this.Difficulty.Controls.Add(this.label2);
+            this.Difficulty.Controls.Add(this.label1);
+            this.Difficulty.Location = new System.Drawing.Point(12, 12);
+            this.Difficulty.Name = "Difficulty";
+            this.Difficulty.Size = new System.Drawing.Size(295, 153);
+            this.Difficulty.TabIndex = 7;
+            this.Difficulty.TabStop = false;
+            this.Difficulty.Text = "groupBox1";
+            this.Difficulty.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 77);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 31);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 54);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Confirm
+            // 
+            this.Confirm.Location = new System.Drawing.Point(145, 213);
+            this.Confirm.Name = "Confirm";
+            this.Confirm.Size = new System.Drawing.Size(75, 23);
+            this.Confirm.TabIndex = 8;
+            this.Confirm.Text = "OK";
+            this.Confirm.UseVisualStyleBackColor = true;
+            // 
+            // Cancel
+            // 
+            this.Cancel.Location = new System.Drawing.Point(226, 213);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(75, 23);
+            this.Cancel.TabIndex = 9;
+            this.Cancel.Text = "Calcel";
+            this.Cancel.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Name : ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 175);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(104, 20);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Text = "Anonymous";
             // 
             // Options
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(319, 252);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.Confirm);
+            this.Controls.Add(this.Difficulty);
             this.Name = "Options";
             this.ShowInTaskbar = false;
             this.Text = "Options";
             this.Load += new System.EventHandler(this.Options_Load);
+            this.Difficulty.ResumeLayout(false);
+            this.Difficulty.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox inputBox1;
+        private System.Windows.Forms.TextBox inputBox2;
+        private System.Windows.Forms.TextBox inputBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton NonStanrdard;
+        private System.Windows.Forms.GroupBox Difficulty;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button Confirm;
+        private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- GradedP4Lab03/Options.cs
+++ GradedP4Lab03/Options.cs
@@ -21,44 +21,14 @@ namespace GradedP4Lab03
         public static int width = 4;
         public static int mines = 1;
 
-        private TextBox inputBox1;
+        /*private TextBox inputBox1;
         private TextBox inputBox2;
         private TextBox inputBox3;
-
+        */
         private void Options_Load(object sender, EventArgs e)
         {
-            this.Width = 220;
-            this.Height = 150;
-            this.FormBorderStyle = FormBorderStyle.FixedSingle;
-
-            this.Text = "Options";
-
-            Label textLabel1 = new Label() { Left = 0, Top = 20, Text = "Height", Width = 40 };
-            inputBox1 = new TextBox() { Left = 50, Top = 20, Width = 120 };
-
-            Label textLabel2 = new Label() { Left = 0, Top = 40, Text = "Width", Width = 40 };
-            inputBox2 = new TextBox() { Left = 50, Top = 40, Width = 120 };
-
-            Label textLabel3 = new Label() { Left = 0, Top = 60, Text = "Mines", Width = 40 };
-            inputBox3 = new TextBox() { Left = 50, Top = 60, Width = 120 };
-
-            Button Confirm  = new Button() { Text = "OK", Left = 10, Width = 80, Top = 85 };
-            Button  cancel = new Button() { Text = "Cancel", Left = 100, Width = 80, Top = 85 };
-
-            cancel.Click += new EventHandler(Cancel_Click);
+            Cancel.Click += new EventHandler(Cancel_Click);
             Confirm.Click += new EventHandler(Confirm_Click);
-
-            this.Controls.Add(Confirm);
-            this.Controls.Add(cancel);
-
-            this.Controls.Add(textLabel1);
-            this.Controls.Add(textLabel2);
-            this.Controls.Add(textLabel3);
-
-            this.Controls.Add(inputBox1);
-            this.Controls.Add(inputBox2);
-            this.Controls.Add(inputBox3);
-
         }
 
         protected void Cancel_Click(object sender, EventArgs e) {
@@ -74,8 +44,8 @@ namespace GradedP4Lab03
             
             try {
                 int test_height = int.Parse(inputBox1.Text);
-                int test_width = int.Parse(inputBox1.Text);
-                int test_mines = int.Parse(inputBox1.Text);
+                int test_width = int.Parse(inputBox2.Text);
+                int test_mines = int.Parse(inputBox3.Text);
                 
                 if (!Enumerable.Range(1, 15).Contains(test_height) || !Enumerable.Range(1, 15).Contains(test_width) || !Enumerable.Range(0, test_height * test_width).Contains(test_mines)) {
                     ApplicationException CustomException = new ApplicationException();  
@@ -89,6 +59,16 @@ namespace GradedP4Lab03
             catch{
                 Show_Error();
             }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
         }
 
 
